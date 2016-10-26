fa2f7c03f11654a11b8cd6ab7df5449e0a5674ad
Davide Gatto <davgatto@gmail.com>
2016-10-26 21:11:24 +0200

    Aggiunge info Copyright

--- MADKing/src/main/java/com/gmail/davgatto/MADKing/Maker/TeacherDetails.java
+++ MADKing/src/main/java/com/gmail/davgatto/MADKing/Maker/TeacherDetails.java
@@ -1,3 +1,26 @@
+/**
+ * 	MADMaker - Genera e invia tramite PEC moduli di MAD per supplenze nelle scuole itailane
+ *  (No chance you need this program and do not speak Italian!)
+ *  
+ *   Copyright (C) 2016  Davide Gatto
+ *   
+ *   @author Davide Gatto
+ *   @mail davgatto@gmail.com
+ *   
+ *   This file is part of MADMaker
+ *   MADMaker is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.gmail.davgatto.MADKing.Maker;
 
 import java.io.FileReader;
